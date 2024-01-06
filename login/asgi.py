8b0aaee4a5c33374f0cdacf62121216d8ff98eff
Natan Ceballos <natan.ceballos@tutanota.com>
2024-01-06 12:12:40 -0500

    setup

--- login/asgi.py
+++ login/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for elevate project.
+ASGI config for login project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elevate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'login.settings')
 
 application = get_asgi_application()
